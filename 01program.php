564399ca34deb724f929551815f0920c694cbe42
MartynaB <martyna.betkowska@gmail.com>
2020-06-09 14:49:56 +0200

    Czwarty commit

--- 01program.php
+++ 01program.php
@@ -1,5 +1,8 @@
 <?php
 echo ("Witaj");
-echo("br>Jestem programem 01");
+echo("<br>Jestem programem 01");
 echo ("<br> Test");
+echo("<br> TEST 11111");
+echo("<br> TEST 2222");
+echo("<br>Dalsza czesc programu");
 ?>
